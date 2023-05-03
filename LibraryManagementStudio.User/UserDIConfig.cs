da7b4d74cfc2ff5f324b247fb1cf4e223354ecfb
mikol <mikolaj.frycowski@gmail.com>
2023-05-04 00:23:28 +0200

    Add DI container configuration for User

---
+++ LibraryManagementStudio.User/UserDIConfig.cs
@@ -0,0 +1,31 @@
+using Autofac;
+using LibraryManagementStudio.Data;
+using LibraryManagementStudio.User.Services;
+using LibraryManagementStudio.User.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementStudio.User;
+
+public static class UserDIConfig
+{
+    public static IContainer Configure()
+    {
+        var builder = new ContainerBuilder();
+
+        //dbContext
+        builder.RegisterType<LibraryDbContext>().AsSelf();
+        builder.RegisterType<DbContextOptions<LibraryDbContext>>().AsSelf().InstancePerLifetimeScope();
+        
+        //services
+        builder.RegisterType<UserAuthService>().As<IUserAuthService>();
+        builder.RegisterType<UserBookService>().As<IUserBookService>();
+        builder.RegisterType<UserBookCopyService>().As<IUserBookCopyService>();
+        builder.RegisterType<UserBookBorrowService>().As<IUserBookBorrowService>();
+        builder.RegisterType<EmailService>().As<IEmailService>();
+        
+        //forms
+        
+
+        return builder.Build();
+    }
+}
