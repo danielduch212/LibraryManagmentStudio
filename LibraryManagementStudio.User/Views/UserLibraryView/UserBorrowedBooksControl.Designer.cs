66129f5b97041eeab163e065a43647fbe5144f0a
mikol <mikolaj.frycowski@gmail.com>
2023-05-06 19:38:39 +0200

    Add UserBorrowedBooksControl

---
+++ LibraryManagementStudio.User/Views/UserLibraryView/UserBorrowedBooksControl.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace LibraryManagementStudio.User.Views.UserLibraryView
+{
+    partial class UserBorrowedBooksControl
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clearSearchButton = new System.Windows.Forms.Button();
+            this.searchBooksButton = new System.Windows.Forms.Button();
+            this.returnBookButton = new System.Windows.Forms.Button();
+            this.searchBooksTextBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.bookListGridView = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bookListGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.clearSearchButton);
+            this.panel1.Controls.Add(this.searchBooksButton);
+            this.panel1.Controls.Add(this.returnBookButton);
+            this.panel1.Controls.Add(this.searchBooksTextBox);
+            this.panel1.Location = new System.Drawing.Point(3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1602, 44);
+            this.panel1.TabIndex = 0;
+            // 
+            // clearSearchButton
+            // 
+            this.clearSearchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearSearchButton.BackColor = System.Drawing.Color.Red;
+            this.clearSearchButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clearSearchButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.clearSearchButton.ForeColor = System.Drawing.Color.White;
+            this.clearSearchButton.Location = new System.Drawing.Point(1164, 2);
+            this.clearSearchButton.Name = "clearSearchButton";
+            this.clearSearchButton.Size = new System.Drawing.Size(29, 29);
+            this.clearSearchButton.TabIndex = 6;
+            this.clearSearchButton.Text = "x";
+            this.clearSearchButton.UseVisualStyleBackColor = false;
+            this.clearSearchButton.Click += new System.EventHandler(this.clearSearchButton_Click);
+            // 
+            // searchBooksButton
+            // 
+            this.searchBooksButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchBooksButton.Location = new System.Drawing.Point(1064, 2);
+            this.searchBooksButton.Name = "searchBooksButton";
+            this.searchBooksButton.Size = new System.Drawing.Size(94, 29);
+            this.searchBooksButton.TabIndex = 5;
+            this.searchBooksButton.Text = "Szukaj";
+            this.searchBooksButton.UseVisualStyleBackColor = true;
+            this.searchBooksButton.Click += new System.EventHandler(this.searchBooksButton_Click);
+            // 
+            // returnBookButton
+            // 
+            this.returnBookButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.returnBookButton.Location = new System.Drawing.Point(1505, 2);
+            this.returnBookButton.Name = "returnBookButton";
+            this.returnBookButton.Size = new System.Drawing.Size(94, 29);
+            this.returnBookButton.TabIndex = 4;
+            this.returnBookButton.Text = "Zwróć";
+            this.returnBookButton.UseVisualStyleBackColor = true;
+            this.returnBookButton.Click += new System.EventHandler(this.returnBookButton_Click);
+            // 
+            // searchBooksTextBox
+            // 
+            this.searchBooksTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchBooksTextBox.Location = new System.Drawing.Point(3, 3);
+            this.searchBooksTextBox.Name = "searchBooksTextBox";
+            this.searchBooksTextBox.Size = new System.Drawing.Size(1055, 27);
+            this.searchBooksTextBox.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.bookListGridView);
+            this.panel2.Location = new System.Drawing.Point(3, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1602, 595);
+            this.panel2.TabIndex = 1;
+            // 
+            // bookListGridView
+            // 
+            this.bookListGridView.AllowUserToAddRows = false;
+            this.bookListGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bookListGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.bookListGridView.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bookListGridView.Location = new System.Drawing.Point(3, 3);
+            this.bookListGridView.Name = "bookListGridView";
+            this.bookListGridView.RowHeadersWidth = 51;
+            this.bookListGridView.RowTemplate.Height = 29;
+            this.bookListGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.bookListGridView.Size = new System.Drawing.Size(1596, 589);
+            this.bookListGridView.TabIndex = 1;
+            // 
+            // UserBorrowedBooksControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "UserBorrowedBooksControl";
+            this.Size = new System.Drawing.Size(1608, 648);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bookListGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox searchBooksTextBox;
+        private Button returnBookButton;
+        private Button searchBooksButton;
+        private DataGridView bookListGridView;
+        private Button clearSearchButton;
+    }
+}
---
+++ LibraryManagementStudio.User/Views/UserLibraryView/UserBorrowedBooksControl.cs
@@ -0,0 +1,146 @@
+﻿using System.ComponentModel;
+using Autofac;
+using LibraryManagementStudio.User.Dtos.BookBorrow;
+using LibraryManagementStudio.User.Dtos.User;
+using LibraryManagementStudio.User.Services.Interfaces;
+
+namespace LibraryManagementStudio.User.Views.UserLibraryView
+{
+    public partial class UserBorrowedBooksControl : UserControl
+    {
+        private readonly UserDto _userDto;
+        private readonly IUserBookBorrowService _userBookBorrowService;
+
+        private List<BookBorrowDto> _bookBorrowsList = null;
+
+        public UserBorrowedBooksControl(UserDto userDto)
+        {
+            InitializeComponent();
+            searchBooksTextBox.KeyPress += CheckEnterKeyPress;
+            
+            _userDto = userDto;
+            
+            var diContainer = UserDIConfig.Configure();
+            _userBookBorrowService = diContainer.Resolve<IUserBookBorrowService>();
+
+            // _bookService = bookService;
+            // _borrowedBookService = borrowedBookService;
+            // _user = user;
+            //RefreshBooks();
+
+            ViewStyleHelper.MaximizeUserControl(this);
+            InitializeView();
+        }
+        
+        private void InitializeView()
+        {
+            searchBooksTextBox.Text = "";
+            _bookBorrowsList = _userBookBorrowService.GetBorrowedBooks(_userDto.UserId).ToList();
+            SetupGridData();
+        }
+
+        private void SearchBooks()
+        {
+            _bookBorrowsList = _userBookBorrowService.GetBorrowedBooks(_userDto.UserId, searchBooksTextBox.Text).ToList();
+            SetupGridData();
+        }
+
+        private void SetupGridData()
+        {
+            var bindingList = new BindingList<BookBorrowDto>(_bookBorrowsList);
+            var books = new BindingSource(bindingList, null);
+            SetupGridViewStyle(books);
+        }
+        
+        private void SetupGridViewStyle(BindingSource books)
+        {
+            bookListGridView.DataSource = books;
+        
+            bookListGridView.Columns["BookBorrowId"]!.Visible = false;
+
+            bookListGridView.Columns["Title"]!.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            bookListGridView.Columns["Title"]!.HeaderText = "Tytuł";
+        
+            bookListGridView.Columns["AuthorName"]!.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            bookListGridView.Columns["AuthorName"]!.HeaderText = "Autor";
+            
+            bookListGridView.Columns["Category"]!.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            bookListGridView.Columns["Category"]!.HeaderText = "Kategoria";
+            
+            bookListGridView.Columns["StartDate"]!.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            bookListGridView.Columns["StartDate"]!.HeaderText = "Od";
+            
+            bookListGridView.Columns["EndDate"]!.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            bookListGridView.Columns["EndDate"]!.HeaderText = "Do";
+
+            bookListGridView.BackgroundColor = Color.White;
+            bookListGridView.RowHeadersVisible = false;
+        }
+
+        private void searchBooksButton_Click(object sender, EventArgs e)
+        {
+            if (searchBooksTextBox.Text != "")
+            {
+                SearchBooks();
+            }
+            else
+            {
+                InitializeView();
+            }
+        }
+        
+        private void clearSearchButton_Click(object sender, EventArgs e)
+        {
+            searchBooksTextBox.Text = "";
+            InitializeView();
+        }
+        
+        private void returnBookButton_Click(object sender, EventArgs e)
+        {
+            // Book selectedBook = (Book)bookListGridView.CurrentRow.DataBoundItem;
+            //
+            // if (selectedBook != null && selectedBook.Status == BookStatus.odebrana)
+            // {
+            //     _borrowedBookService.ReturnBook(selectedBook, _user.Id);
+            //     RefreshBooks();
+            //     SetupView();
+            //     MessageBox.Show("Zwrot został zaakceptowany.");
+            // }
+            // else if (selectedBook != null && selectedBook.Status == BookStatus.nieodebrana)
+            // {
+            //     MessageBox.Show("Nie można zwrócić nie odebranej książki.");
+            // }
+        }
+        
+        // private void SearchBooks()
+        // {
+        //     List<Book> booksList = _bookList.Where(b => b.Title.ToLower().Contains(searchBooksTextBox.Text.ToLower())).ToList();
+        //     var bindingList = new BindingList<Book>(booksList);
+        //     var books = new BindingSource(bindingList, null);
+        //     SetupGridViewStyle(books);
+        // }
+        //
+        // private void SetupView()
+        // {
+        //     var bindingList = new BindingList<Book>(_bookList);
+        //     var books = new BindingSource(bindingList, null);
+        //     SetupGridViewStyle(books);
+        // }
+        //
+        //
+        //
+        // private void RefreshBooks()
+        // {
+        //     var books = _bookService.GetBooks().ToList();
+        //     _bookList = books.Where(x => x.RentierId == _user.Id).ToList();
+        // }
+        //
+        private void CheckEnterKeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                SearchBooks();
+            }
+        }
+    }
+}
