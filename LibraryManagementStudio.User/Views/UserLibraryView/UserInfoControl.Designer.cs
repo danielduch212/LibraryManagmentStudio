1741b022773d46170df8bea2bbaa74d7289ebbaf
mikol <mikolaj.frycowski@gmail.com>
2023-05-06 19:38:25 +0200

    Add UserInfo control

---
+++ LibraryManagementStudio.User/Views/UserLibraryView/UserInfoControl.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace LibraryManagementStudio.User.Views.UserLibraryView
+{
+    partial class UserInfoControl
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            CountryTextBox = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            cityTextBox = new TextBox();
+            label3 = new Label();
+            postalCodeTextBox = new TextBox();
+            label4 = new Label();
+            birthdateTextBox = new TextBox();
+            label5 = new Label();
+            nameTextBox = new TextBox();
+            label6 = new Label();
+            surnameTextBox = new TextBox();
+            label7 = new Label();
+            emailTextBox = new TextBox();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            panel4 = new Panel();
+            label12 = new Label();
+            streetTextBox = new TextBox();
+            label15 = new Label();
+            accountCreationDateTextBox = new TextBox();
+            label11 = new Label();
+            currentBorrowsTextBox = new TextBox();
+            label13 = new Label();
+            panel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // CountryTextBox
+            // 
+            CountryTextBox.Location = new Point(437, 94);
+            CountryTextBox.Name = "CountryTextBox";
+            CountryTextBox.Size = new Size(354, 27);
+            CountryTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(437, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Kraj:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(437, 137);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Miasto:";
+            // 
+            // cityTextBox
+            // 
+            cityTextBox.Location = new Point(437, 160);
+            cityTextBox.Name = "cityTextBox";
+            cityTextBox.Size = new Size(354, 27);
+            cityTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(437, 204);
+            label3.Name = "label3";
+            label3.Size = new Size(107, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Kod pocztowy:";
+            // 
+            // postalCodeTextBox
+            // 
+            postalCodeTextBox.Location = new Point(437, 227);
+            postalCodeTextBox.Name = "postalCodeTextBox";
+            postalCodeTextBox.Size = new Size(354, 27);
+            postalCodeTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(18, 268);
+            label4.Name = "label4";
+            label4.Size = new Size(114, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Data urodzenia:";
+            // 
+            // birthdateTextBox
+            // 
+            birthdateTextBox.Location = new Point(18, 291);
+            birthdateTextBox.Name = "birthdateTextBox";
+            birthdateTextBox.Size = new Size(354, 27);
+            birthdateTextBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(18, 71);
+            label5.Name = "label5";
+            label5.Size = new Size(41, 20);
+            label5.TabIndex = 9;
+            label5.Text = "Imię:";
+            // 
+            // nameTextBox
+            // 
+            nameTextBox.Location = new Point(18, 94);
+            nameTextBox.Name = "nameTextBox";
+            nameTextBox.Size = new Size(354, 27);
+            nameTextBox.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(18, 137);
+            label6.Name = "label6";
+            label6.Size = new Size(75, 20);
+            label6.TabIndex = 11;
+            label6.Text = "Nazwisko:";
+            // 
+            // surnameTextBox
+            // 
+            surnameTextBox.Location = new Point(18, 160);
+            surnameTextBox.Name = "surnameTextBox";
+            surnameTextBox.Size = new Size(354, 27);
+            surnameTextBox.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(18, 204);
+            label7.Name = "label7";
+            label7.Size = new Size(91, 20);
+            label7.TabIndex = 13;
+            label7.Text = "Adres email:";
+            // 
+            // emailTextBox
+            // 
+            emailTextBox.Location = new Point(18, 227);
+            emailTextBox.Name = "emailTextBox";
+            emailTextBox.Size = new Size(354, 27);
+            emailTextBox.TabIndex = 12;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(437, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(172, 25);
+            label8.TabIndex = 14;
+            label8.Text = "Adres zamieszkania";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(18, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(226, 25);
+            label9.TabIndex = 15;
+            label9.Text = "Informacje o użytkowniku";
+            // 
+            // label10
+            // 
+            label10.Anchor = AnchorStyles.Bottom;
+            label10.AutoSize = true;
+            label10.Enabled = false;
+            label10.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(133, 709);
+            label10.Name = "label10";
+            label10.Size = new Size(717, 17);
+            label10.TabIndex = 16;
+            label10.Text = "Użytkownik może mięc maksymalnie wypożyczone 2 książki oraz maksymalnie zarezerwowane 3 książki w danym momenci";
+            // 
+            // panel4
+            // 
+            panel4.Anchor = AnchorStyles.Top;
+            panel4.Controls.Add(label12);
+            panel4.Controls.Add(streetTextBox);
+            panel4.Controls.Add(label15);
+            panel4.Controls.Add(accountCreationDateTextBox);
+            panel4.Controls.Add(label11);
+            panel4.Controls.Add(currentBorrowsTextBox);
+            panel4.Controls.Add(label13);
+            panel4.Controls.Add(label8);
+            panel4.Controls.Add(CountryTextBox);
+            panel4.Controls.Add(label9);
+            panel4.Controls.Add(label1);
+            panel4.Controls.Add(nameTextBox);
+            panel4.Controls.Add(cityTextBox);
+            panel4.Controls.Add(label5);
+            panel4.Controls.Add(label2);
+            panel4.Controls.Add(label7);
+            panel4.Controls.Add(postalCodeTextBox);
+            panel4.Controls.Add(surnameTextBox);
+            panel4.Controls.Add(label3);
+            panel4.Controls.Add(label6);
+            panel4.Controls.Add(birthdateTextBox);
+            panel4.Controls.Add(emailTextBox);
+            panel4.Controls.Add(label4);
+            panel4.Location = new Point(92, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(815, 694);
+            panel4.TabIndex = 0;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label12.Location = new Point(18, 399);
+            label12.Name = "label12";
+            label12.Size = new Size(175, 25);
+            label12.TabIndex = 26;
+            label12.Text = "Informacje o koncie";
+            // 
+            // streetTextBox
+            // 
+            streetTextBox.Location = new Point(437, 291);
+            streetTextBox.Name = "streetTextBox";
+            streetTextBox.Size = new Size(354, 27);
+            streetTextBox.TabIndex = 24;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(437, 268);
+            label15.Name = "label15";
+            label15.Size = new Size(42, 20);
+            label15.TabIndex = 25;
+            label15.Text = "Ulica";
+            // 
+            // accountCreationDateTextBox
+            // 
+            accountCreationDateTextBox.Location = new Point(18, 489);
+            accountCreationDateTextBox.Name = "accountCreationDateTextBox";
+            accountCreationDateTextBox.Size = new Size(354, 27);
+            accountCreationDateTextBox.TabIndex = 18;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(18, 466);
+            label11.Name = "label11";
+            label11.Size = new Size(162, 20);
+            label11.TabIndex = 19;
+            label11.Text = "Data utworzenia konta:";
+            // 
+            // currentBorrowsTextBox
+            // 
+            currentBorrowsTextBox.Location = new Point(18, 555);
+            currentBorrowsTextBox.Name = "currentBorrowsTextBox";
+            currentBorrowsTextBox.Size = new Size(354, 27);
+            currentBorrowsTextBox.TabIndex = 20;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(18, 532);
+            label13.Name = "label13";
+            label13.Size = new Size(264, 20);
+            label13.TabIndex = 21;
+            label13.Text = "Ilość aktualnie wypożyczonych książek:";
+            // 
+            // UserInfoControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            Controls.Add(panel4);
+            Controls.Add(label10);
+            Name = "UserInfoControl";
+            Size = new Size(1004, 778);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox CountryTextBox;
+        private Label label1;
+        private Label label2;
+        private TextBox cityTextBox;
+        private Label label3;
+        private TextBox postalCodeTextBox;
+        private Label label4;
+        private TextBox birthdateTextBox;
+        private Label label5;
+        private TextBox nameTextBox;
+        private Label label6;
+        private TextBox surnameTextBox;
+        private Label label7;
+        private TextBox emailTextBox;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Panel panel4;
+        private Label label12;
+        private TextBox streetTextBox;
+        private Label label15;
+        private TextBox accountCreationDateTextBox;
+        private Label label11;
+        private TextBox currentBorrowsTextBox;
+        private Label label13;
+    }
+}
---
+++ LibraryManagementStudio.User/Views/UserLibraryView/UserInfoControl.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using LibraryManagementStudio.User.Dtos.User;
+using LibraryManagementStudio.User.Services.Interfaces;
+
+namespace LibraryManagementStudio.User.Views.UserLibraryView
+{
+    public partial class UserInfoControl : UserControl
+    {
+        private readonly UserDto _userDto;
+        private readonly IUserBookBorrowService _userBookBorrowService;
+
+        public UserInfoControl(UserDto userDto)
+        {
+            InitializeComponent();
+            
+            _userDto = userDto;
+            
+            var diContainer = UserDIConfig.Configure();
+            _userBookBorrowService = diContainer.Resolve<IUserBookBorrowService>();
+
+            ViewStyleHelper.MaximizeUserControl(this);
+            SetupView();
+        }
+
+        void SetupView()
+        {
+            nameTextBox.Text = _userDto.FirstName;
+            surnameTextBox.Text = _userDto.LastName;
+            birthdateTextBox.Text = _userDto.DateOfBirth.ToString("dd/MM/yyyy");
+            emailTextBox.Text = _userDto.EmailAddress;
+            cityTextBox.Text = _userDto.City;
+            streetTextBox.Text = _userDto.Street;
+            CountryTextBox.Text = _userDto.Country;
+            postalCodeTextBox.Text = _userDto.PostalCode;
+            accountCreationDateTextBox.Text = _userDto.CreationDate.ToString("dd/MM/yyyy");
+            
+            var borrowsCount = _userBookBorrowService.GetBorrowedBooksCount(_userDto.UserId);
+            currentBorrowsTextBox.Text = borrowsCount.ToString();
+        }
+    }
+}
