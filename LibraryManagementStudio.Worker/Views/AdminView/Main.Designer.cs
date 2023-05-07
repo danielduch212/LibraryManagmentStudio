9bd25a57a3e2e0653595e960aa1a80fc4f866828
danielduch212 <danielduchnowicz1@gmail.com>
2023-05-07 15:23:37 +0200

    essa1

--- LibraryManagementStudio.Worker/Views/AdminView/Main.Designer.cs
+++ LibraryManagementStudio.Worker/Views/AdminView/Main.Designer.cs
@@ -28,19 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.timeLabel);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(217, 581);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(217, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(725, 581);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Użytkownicy";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(369, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(40, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 30);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ADMINISTRATOR";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Size = new System.Drawing.Size(136, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Administrator";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Magazyn książek";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(214, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Dodaj książkę";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 179);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(214, 30);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Generowanie Raportów";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 511);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(214, 30);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Wyloguj";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.timeLabel.Location = new System.Drawing.Point(138, 557);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(40, 15);
+            this.timeLabel.TabIndex = 9;
+            this.timeLabel.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 557);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = ":Czas trwania sesji";
             // 
             // Main
             // 
@@ -48,18 +146,29 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(942, 581);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Main";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.Text = "Main";
             this.Load += new System.EventHandler(this.Main_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private Panel panel1;
+        private Label label2;
+        private Label timeLabel;
+        private Button button7;
+        private Button button4;
+        private Button button3;
+        private Button button2;
         private Label label1;
+        private Button button1;
+        private Panel panel2;
     }
 }
--- LibraryManagementStudio.Worker/Views/AdminView/Main.cs
+++ LibraryManagementStudio.Worker/Views/AdminView/Main.cs
@@ -1,20 +1,27 @@
-﻿namespace LibraryManagementStudio.Worker.Views.AdminView
+﻿using System;
+using System.Timers;
+
+namespace LibraryManagementStudio.Worker.Views.AdminView
 {
     public partial class Main : Form
     {
+
         public Main()
         {
             InitializeComponent();
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void Main_Load(object sender, EventArgs e)
         {
 
         }
+
+       
     }
 }
