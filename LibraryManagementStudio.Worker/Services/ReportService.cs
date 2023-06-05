3f60fd469ddac61d515c3feefa202362a487c0fe
danielduch212 <danielduchnowicz1@gmail.com>
2023-06-05 17:28:46 +0200

    Reports Added

--- LibraryManagementStudio.Worker/Services/ReportService.cs
+++ LibraryManagementStudio.Worker/Services/ReportService.cs
@@ -2,6 +2,8 @@ using LibraryManagementStudio.Data;
 using LibraryManagementStudio.Data.Models;
 using LibraryManagementStudio.Data.Models.Enums;
 using LibraryManagementStudio.Worker.Dtos.ReportData;
+using System.Net;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace LibraryManagementStudio.Worker.Services;
 
@@ -17,16 +19,59 @@ public class ReportService
     }
 
 
-    //public List<ReportData1> returnData(User user, DateTime dateFrom, DateTime dateTo)
-    //{
+    public List<ReportData1> returnData(int userId, DateTime dateFrom, DateTime dateTo)
+    {
+        var query = _dbContext.BookBorrows
+            .Where(x => x.UserId==userId && x.StartDate >= dateFrom && x.StartDate <= dateTo);
+        var bookBorrows = query.Select(x => new ReportData1()
+        {
+            BookBorrowId = x.BookBorrowId,
+            Title = x.BookCopy.Book.Title,
+            Status = x.Status,
+            UserId = x.UserId,
+        });
 
-    //    return 
-    //}
+        return bookBorrows.ToList();
+    }
 
-    //public List<ReportData2> returnData(Author author, Publisher publisher, bool isActive, CategoryType category)
-   // {
+    public List<ReportData2> returnData(Author author, Publisher publisher, bool isActive, CategoryType category)
+    {
+        var query = _dbContext.Books
+            .Where(x => x.Author == author && x.Publisher == publisher && x.IsActive == isActive && x.Category == category);
+        var data = query.Select(x => new ReportData2()
+        {
+            BookId = x.BookId,
+            Title = x.Title,
+            Description = x.Description,
+            AllTimeBookBorrowsCount = CountAllBorrows(x.BookId),
 
-    //}
 
 
+        });
+        return data.ToList();
+    }
+
+    public int CountAllBorrows(int bookId)
+    {
+        var query = _dbContext.BookBorrows
+            .Where(x => x.BookCopy.BookId.Equals(bookId));
+        return query.Count();
+
+    }
+
+    public Author findAuthor(string author)
+    {
+
+        var query = _dbContext.Authors
+            .FirstOrDefault(x => x.Name.Equals(author));
+        return query;
+    }
+
+    public Publisher findPublisher(string publisher)
+    {
+
+        var query = _dbContext.Publishers
+            .FirstOrDefault(x => x.Name.Equals(publisher));
+        return query;
+    }
 }
--- LibraryManagementStudio.Worker/Views/AdminView/Reports.cs
+++ LibraryManagementStudio.Worker/Views/AdminView/Reports.cs
@@ -1,4 +1,7 @@
 ﻿using LibraryManagementStudio.Data;
+using LibraryManagementStudio.Data.Models.Enums;
+using LibraryManagementStudio.Worker.Dtos.ReportData;
+using LibraryManagementStudio.Worker.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,10 +17,16 @@ namespace LibraryManagementStudio.Worker.Views.AdminView
     public partial class Reports : UserControl
     {
         LibraryManagementStudio.Data.Models.Worker worker;
+        ReportService reportService;
         LibraryDbContext dbContext;
+        List<ReportData1> reportData1;
+        List<ReportData2> reportData2;
+        LibraryManagementStudio.Data.Models.Enums.CategoryType categoryType;
+        bool availibility;
         public Reports(Data.Models.Worker worker, LibraryDbContext dbContext)
         {
             InitializeComponent();
+            reportService = new ReportService(dbContext);
             ViewStyleHelper.MaximizeUserControl(this);
             this.dbContext = dbContext;
             this.worker = worker;
@@ -43,12 +52,55 @@ namespace LibraryManagementStudio.Worker.Views.AdminView
 
         private void buttonGenerate1_Click(object sender, EventArgs e)
         {
+            reportData1 = reportService.returnData(Int32.Parse(textBoxUserId.Text), dataTimePickerFrom.Value, dataTimePickerTo.Value);
+            var bindingList = new BindingList<ReportData1>(reportData1);
+            var bindingSource = new BindingSource(bindingList, null);
 
+            dataGridView.DataSource = bindingSource;
+
+            dataGridView.Columns["BookBorrowId"]!.HeaderText = "Id wypozyczenia";
+            dataGridView.Columns["Title"]!.HeaderText = "Tytul";
+            dataGridView.Columns["Status"]!.HeaderText = "Status";
+            dataGridView.Columns["UserId"]!.HeaderText = "Id Uzytkownika";
+            DataGridViewRow summaryRow = new DataGridViewRow();
+            summaryRow.CreateCells(dataGridView);
+            summaryRow.Cells[0].Value = "Ilosc wszystkich wypozyczonych ksiazek w podanym okresie: ";
+            summaryRow.Cells[1].Value = reportData1.Count.ToString();
+
+
+            dataGridView.BackgroundColor = Color.White;
+            dataGridView.RowHeadersVisible = false;
+            dataGridView.Visible = true;
         }
 
         private void buttonGenerate2_Click(object sender, EventArgs e)
         {
+            if (comboBoxCategory.Text == "Romance")
+                categoryType = LibraryManagementStudio.Data.Models.Enums.CategoryType.Romance;
+            if (comboBoxCategory.Text == "Fantasy")
+                categoryType = LibraryManagementStudio.Data.Models.Enums.CategoryType.Fantasy;
+            if (comboBoxCategory.Text == "Bibliography")
+                categoryType = LibraryManagementStudio.Data.Models.Enums.CategoryType.Bibliography;
+            if (comboBoxCategory.Text == "History")
+                categoryType = LibraryManagementStudio.Data.Models.Enums.CategoryType.History;
+            if (comboBoxCategory.Text == "Thriller")
+                categoryType = LibraryManagementStudio.Data.Models.Enums.CategoryType.Thriller;
+            availibility = bool.Parse(comboBoxAvailibility.Text);
+            reportData2 = reportService.returnData(reportService.findAuthor(comboBoxAuthor.Text), reportService.findPublisher(comboBoxPublisher.Text), availibility, categoryType);
 
+            var bindingList = new BindingList<ReportData2>(reportData2);
+            var bindingSource = new BindingSource(bindingList, null);
+
+            dataGridView.DataSource = bindingSource;
+
+            dataGridView.Columns["BookId"]!.HeaderText = "Id Ksiazki";
+            dataGridView.Columns["Title"]!.HeaderText = "Tytul";
+            dataGridView.Columns["Description"]!.HeaderText = "Opis";
+            dataGridView.Columns["AllTimeBookBorrowsCount"]!.HeaderText = "Wszystkie wypozyczenia";
+
+            dataGridView.BackgroundColor = Color.White;
+            dataGridView.RowHeadersVisible = false;
+            dataGridView.Visible = true;
         }
 
         private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
