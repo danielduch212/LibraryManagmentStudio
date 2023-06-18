c4e8c957680958b6df8a75298bd71d991bfed1c8
mikol <mikolaj.frycowski@gmail.com>
2023-06-18 17:11:16 +0200

    fix penalties

--- LibraryManagementStudio.User/Views/UserLibraryView/UserPenaltiesControl.cs
+++ LibraryManagementStudio.User/Views/UserLibraryView/UserPenaltiesControl.cs
@@ -35,45 +35,50 @@ namespace LibraryManagementStudio.User.Views.UserLibraryView
             _paidPenalties = _userPenaltyService.GetPenalties(_userDto.UserId, true).ToList();
             var paidBindingList = new BindingList<PenaltyDto>(_paidPenalties);
             var paidBindingSource = new BindingSource(paidBindingList, null);
-            getCodesDataGridView.DataSource = paidBindingSource;
-            
+            returnCodesDataGridView.DataSource = paidBindingSource;
+    
             _unPaidPenalties = _userPenaltyService.GetPenalties(_userDto.UserId, false).ToList();
             var unPaidBindingList = new BindingList<PenaltyDto>(_unPaidPenalties);
             var unPaidBindingSource = new BindingSource(unPaidBindingList, null);
-            returnCodesDataGridView.DataSource = unPaidBindingSource;
+            getCodesDataGridView.DataSource = unPaidBindingSource;
         }
 
         private void SetupPickupGridViewStyle()
         {
-            // getCodesDataGridView.Columns["Id"].Visible = false;
-            // getCodesDataGridView.Columns["UserId"].Visible = false;
-            // getCodesDataGridView.Columns["CodeType"].Visible = false;
-            // getCodesDataGridView.Columns["BorrowedBookId"].Visible = false;
-            //
-            // getCodesDataGridView.Columns["Code"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            // getCodesDataGridView.Columns["Code"].HeaderText = "Kod";
-            //
-            // getCodesDataGridView.Columns["BookName"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            // getCodesDataGridView.Columns["BookName"].HeaderText = "Tytuł książki";
-            //
-            // getCodesDataGridView.RowHeadersVisible = false;
+            getCodesDataGridView.Columns["ImpositionDate"].Visible = false;
+
+            getCodesDataGridView.Columns["Description"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            getCodesDataGridView.Columns["Description"].HeaderText = "Opis";
+            
+            getCodesDataGridView.Columns["PenaltyId"].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            getCodesDataGridView.Columns["PenaltyId"].HeaderText = "Id";
+            
+            getCodesDataGridView.Columns["Price"].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            getCodesDataGridView.Columns["Price"].HeaderText = "Koszt";      
+            
+            getCodesDataGridView.Columns["BookTitle"].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            getCodesDataGridView.Columns["BookTitle"].HeaderText = "Tytuł książki";
+            
+            getCodesDataGridView.RowHeadersVisible = false;
         }
 
         private void SetupReturnGridViewStyle()
         {
+            returnCodesDataGridView.Columns["ImpositionDate"].Visible = false;
 
-            // returnCodesDataGridView.Columns["Id"].Visible = false;
-            // returnCodesDataGridView.Columns["UserId"].Visible = false;
-            // returnCodesDataGridView.Columns["CodeType"].Visible = false;
-            // returnCodesDataGridView.Columns["BorrowedBookId"].Visible = false;
-            //
-            // returnCodesDataGridView.Columns["Code"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            // returnCodesDataGridView.Columns["Code"].HeaderText = "Kod";
-            //
-            // returnCodesDataGridView.Columns["BookName"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            // returnCodesDataGridView.Columns["BookName"].HeaderText = "Tytuł książki";
-            //
-            // returnCodesDataGridView.RowHeadersVisible = false;
+            returnCodesDataGridView.Columns["Description"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            returnCodesDataGridView.Columns["Description"].HeaderText = "Opis";
+            
+            returnCodesDataGridView.Columns["PenaltyId"].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            returnCodesDataGridView.Columns["PenaltyId"].HeaderText = "Id";
+            
+            returnCodesDataGridView.Columns["Price"].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            returnCodesDataGridView.Columns["Price"].HeaderText = "Koszt";      
+            
+            returnCodesDataGridView.Columns["BookTitle"].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            returnCodesDataGridView.Columns["BookTitle"].HeaderText = "Tytuł książki";
+            
+            returnCodesDataGridView.RowHeadersVisible = false;
         }
     }
 }
